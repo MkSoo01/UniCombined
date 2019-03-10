bb2902333c66c066a71366bdf3736bd70096b4ae
MkSoo01 <khimsoo01@gmail.com>
2019-03-10 13:23:08 +0800

    addUniversity (backend part)

--- js/add-university.js
+++ js/add-university.js
@@ -1,8 +1,9 @@
 var uniName = document.getElementById("uniName");
 var description = document.getElementById("description");
 var adminNo = document.getElementById("adminNo");
+var img = document.getElementById("uniImg");
 var errorMsg = document.getElementsByTagName("p");
-var invalidUniName = invalidDescription  = invalidAdminNo = false;
+var invalidUniName = invalidDescription  = invalidAdminNo = invalidImg = false;
 			
 uniName.onkeyup = function(){
 	if (uniName.value != ""){
@@ -24,7 +25,20 @@ function idTypeSelect(){
 		adminNo.style.border = "1px solid lightgrey";
 	}
 }
-			
+var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;		
+function imgValidation(){
+	if (!allowedExtensions.test(img.value)){
+		errorMsg[4].innerHTML = "&#10007;<small> Please upload image file having extension .jpeg/.jpg/.png/.gif only</small>";
+		errorMsg[4].style.display = "block";
+		img.style.border = "1px solid red";
+		invalidImg = true;
+	}else{
+		errorMsg[4].style.display = "none";
+		img.style.border = "1px solid lightgrey";
+		invalidImg = false;
+	}
+}
+
 //addUni function will be called when the next button is clicked
 function addUni(){
 	//check if the university name is blank, if it is, display error message
@@ -50,10 +64,17 @@ function addUni(){
 		invalidAdminNo = true;
 	}else
 		invalidAdminNo = false;
-
+	
+	if (img.value == ""){
+		invalidImg = true;
+		errorMsg[4].innerHTML = "&#10007;<small> Please upload an university image</small>";
+		errorMsg[4].style.display = "block";
+		img.style.border = "1px solid red";
+	}
 	//alert("a"+ invalidUsername + "" + invalidPsw + "" + invalidCPsw + "" + invalidIDtype + "" + invalidIDno + "" + invalidName + "" +
 	//invalidNationality + "" + invalidDOB + "" + invalidEmail + "" + invalidPhoneNo + "" + invalidAddress);
-	invalid = [invalidUniName, invalidDescription, invalidAdminNo];
+	//alert("" + invalidImg);
+	invalid = [invalidUniName, invalidDescription, invalidAdminNo, invalidImg];
 	for ( i = 0 ; i < invalid.length; i++){
 		if (invalid[i]){
 			document.getElementsByClassName("form-control")[i].focus();
